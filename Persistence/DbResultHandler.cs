4f34d1d5de62316c41218cbf0911b1daddd7fac5
Balamurali Pandranki <balamurali@live.com>
2021-01-07 10:39:17 +0530

    Add Classes

---
+++ Persistence/DbResultHandler.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace QuantConnect.Persistence
+{
+    public class DbResultHandler
+    {
+      
+    }
+}
---
+++ Persistence/Models/LeanContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace QuantConnect.Persistence.Models
+{
+    public partial class LeanDbContext : DbContext
+    {
+        public LeanDbContext()
+        {
+        }
+
+        public LeanDbContext(DbContextOptions<LeanDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("Host=localhost;Database=Lean;Username=postgres;Password=postgres");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
