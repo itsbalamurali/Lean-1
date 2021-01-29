c01287440e204967b390cadeba71179477e1b449
Aman Ray <ray.aman9876@gmail.com>
2021-01-29 06:56:49 +0000

    CCXT Kraken Init

---
+++ Brokerages/Kraken/KrakenBrokerageFactory.cs
@@ -0,0 +1,91 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using QuantConnect.Configuration;
+using QuantConnect.Data;
+using QuantConnect.Interfaces;
+using QuantConnect.Securities;
+using QuantConnect.Util;
+
+namespace QuantConnect.Brokerages.Kraken
+{
+    /// <summary>
+    /// Factory method to create binance Websockets brokerage
+    /// </summary>
+    public class KrakenBrokerageFactory : BrokerageFactory
+    {
+        /// <summary>
+        /// Factory constructor
+        /// </summary>
+        public KrakenBrokerageFactory() : base(typeof(KrakenBrokerage))
+        {
+        }
+
+        /// <summary>
+        /// Not required
+        /// </summary>
+        public override void Dispose()
+        {
+        }
+
+        /// <summary>
+        /// provides brokerage connection data
+        /// </summary>
+        public override Dictionary<string, string> BrokerageData => new Dictionary<string, string>
+        {
+            { "kraken-connect-key", Config.Get("kraken-connect-key")},
+            { "kraken-secret-key", Config.Get("kraken-secret-key")},
+            { "kraken-key-name", Config.Get("kraken-key-name")}
+        };
+
+        /// <summary>
+        /// The brokerage model
+        /// </summary>
+        /// <param name="orderProvider">The order provider</param>
+        public override IBrokerageModel GetBrokerageModel(IOrderProvider orderProvider) => new KrakenBrokerageModel();
+
+        /// <summary>
+        /// Create the Brokerage instance
+        /// </summary>
+        /// <param name="job"></param>
+        /// <param name="algorithm"></param>
+        /// <returns></returns>
+        public override IBrokerage CreateBrokerage(Packets.LiveNodePacket job, IAlgorithm algorithm)
+        {
+            var required = new[] { "kraken-connect-key", "kraken-secret-key", "kraken-key-name" };
+
+            foreach (var item in required)
+            {
+                if (string.IsNullOrEmpty(job.BrokerageData[item]))
+                {
+                    throw new Exception($"KrakenBrokerageFactory.CreateBrokerage: Missing {item} in config.json");
+                }
+            }
+
+            var brokerage = new KrakenBrokerage(
+                job.BrokerageData["kraken-connect-key"],
+                job.BrokerageData["kraken-secret-key"],
+                job.BrokerageData["kraken-key-name"],
+                
+                algorithm,
+                Composer.Instance.GetExportedValueByTypeName<IDataAggregator>(Config.Get("data-aggregator", "QuantConnect.Lean.Engine.DataFeeds.AggregationManager")));
+            Composer.Instance.AddPart<IDataQueueHandler>(brokerage);
+
+            return brokerage;
+        }
+    }
+}
